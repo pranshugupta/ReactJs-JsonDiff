1c3f822fa9e4d814d60bc92f779ee9e39d07c3b1
pranshugupta <pranshugupta18@gmail.com>
2022-01-06 19:37:16 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style.css";
-import App from "./App";
-import JsonDiff "./jsonDiff/JsonDiff"
+import JsonDiff from "./jsonDiff/JsonDiff";
 export default function App() {
   return (
     <JsonDiff/>
